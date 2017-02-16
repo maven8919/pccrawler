fd0f2996650532f414ad646736b5b89b9e9de8d6
Levente Bakos <levente_bakos@epam.com>
2017-02-16 10:48:36 +0100

    Initial commit

---
+++ src/main/java/com/maven8919/PccrawlerApplication.java
@@ -0,0 +1,12 @@
+package com.maven8919;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PccrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PccrawlerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/maven8919/PccrawlerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.maven8919;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PccrawlerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
